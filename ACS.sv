b3429038d897edd86ff94ad0e7c03f8d38302150
mmerioles <matthewmerioles@yahoo.com>
2023-03-22 16:46:20 -0700

    All files

---
+++ ACS.sv
@@ -0,0 +1,20 @@
+module ACS		                        // add-compare-select
+(
+   input       path_0_valid,
+   input       path_1_valid,
+   input [1:0] path_0_bmc,	            // branch metric computation
+   input [1:0] path_1_bmc,				
+   input [7:0] path_0_pmc,				// path metric computation
+   input [7:0] path_1_pmc,
+
+   output logic        selection,
+   output logic        valid_o,
+   output      [7:0] path_cost);  
+
+   wire  [7:0] path_cost_0;			   // branch metric + path metric
+   wire  [7:0] path_cost_1;
+
+/* Fill in the guts per ACS instructions
+*/
+
+endmodule
